a13e47248adf9530ace5bf3655aafeef2df60036
The-Box-Lord <56173725+The-Box-Lord@users.noreply.github.com>
2021-04-02 23:53:50 -0500

    Add speakers + add contact us section

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function signUp() {
-    window.open("https://forms.gle/tqMPK4SbVYsvMjaw9");
+    window.open('https://forms.gle/tqMPK4SbVYsvMjaw9');
+}
+
+function lol() {
+    alert("LOL this thing isn't done yet");
+    window.open('https://youtu.be/dQw4w9WgXcQ');
 }
